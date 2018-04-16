29b2a9ee3b8f3a79f2b9ccfece934668b57e7c39
UTurista <vasckko@gmail.com>
2018-04-16 15:57:33 +0100

    Updated (unused) news model class to be used for the RSS feed news

--- app/src/main/java/pt/uturista/prspy/model/News.java
+++ app/src/main/java/pt/uturista/prspy/model/News.java
@@ -16,12 +16,86 @@
 
 package pt.uturista.prspy.model;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.support.annotation.IntDef;
+import android.support.annotation.StringDef;
 
-import pt.uturista.bf2.NewsJson;
+import com.rometools.rome.feed.synd.SyndContent;
+import com.rometools.rome.feed.synd.SyndEntry;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.Date;
+
+import pt.uturista.prspy.R;
 
 public class News implements Parcelable {
+    public final static int EVENT = 1;
+    public final static int TOURNAMENT = 2;
+    public final static int OFFICIAL = 3;
+    public static final int BLOG = 4;
+    public static final int HIGHTLIGHTS = 5;
+
+
+    private final String mTitle;
+    private final String mContent;
+    private final int mType;
+    private final Date mDate;
+    private final String mAuthor;
+
+    public News(SyndEntry entry, @NewsTypes int type) {
+        mTitle = entry.getTitle();
+        mContent = entry.getContents().get(0).getValue();
+        mDate = entry.getPublishedDate();
+        mAuthor = entry.getAuthor();
+        mType = type;
+    }
+
+    @Override
+    public String toString() {
+        return mType + " " + mTitle + " " + mContent;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+    public String getContent() {
+        return mContent;
+    }
+    public Date getPublishedDate() {
+        return mDate;
+    }
+
+    /* *********************************
+     *         Parcelable Region
+     * ********************************* */
+
+    protected News(Parcel in) {
+        mTitle = in.readString();
+        mContent = in.readString();
+        mDate = new Date(in.readLong());
+        mAuthor = in.readString();
+        mType = in.readInt();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mTitle);
+        dest.writeString(mContent);
+        dest.writeLong(mDate.getTime());
+        dest.writeString(mAuthor);
+        dest.writeInt(mType);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
     public static final Creator<News> CREATOR = new Creator<News>() {
         @Override
         public News createFromParcel(Parcel in) {
@@ -33,33 +107,40 @@ public class News implements Parcelable {
             return new News[size];
         }
     };
-    private static final String EVENT = "#999999";
-    private static final String BLOG = "#3399ff";
-    private final String text;
-    private final String url;
-    private final String color;
 
-    public News(NewsJson json) {
-        text = json.text;
-        url = json.url;
-        color = json.color;
+    public String getAuthor() {
+        return mAuthor;
     }
 
-    protected News(Parcel in) {
-        text = in.readString();
-        url = in.readString();
-        color = in.readString();
+    public int getType() {
+        return mType;
     }
 
-    @Override
-    public int describeContents() {
-        return 0;
+    public String getLocalizedType(Context context) {
+        switch (getType()) {
+            case News.EVENT:
+                return context.getString(R.string.news_type_event);
+
+            case News.TOURNAMENT:
+                return context.getString(R.string.news_type_tournament);
+
+            case News.BLOG:
+                return context.getString(R.string.news_type_blog);
+
+            case News.HIGHTLIGHTS:
+                return context.getString(R.string.news_type_highlights);
+
+            default:
+            case News.OFFICIAL:
+                return context.getString(R.string.news_type_official);
+        }
     }
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(text);
-        dest.writeString(url);
-        dest.writeString(color);
+    /* *********************************
+     *         Fake Enum Region
+     * ********************************* */
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({EVENT, TOURNAMENT, OFFICIAL,BLOG})
+    public @interface NewsTypes {
     }
 }
